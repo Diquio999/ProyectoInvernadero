3848661d0856986236a2fcc832f1ae37d1c05481
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:44:39 +0200

    Update TratamientoArbolRepositorio.java

--- src/main/java/com/dawes/repositorio/TratamientoArbolRepositorio.java
+++ src/main/java/com/dawes/repositorio/TratamientoArbolRepositorio.java
@@ -4,15 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.dawes.modelo.ArbolVO;
 import com.dawes.modelo.TratamientoArbolVO;
-
-
 @Repository
-public interface TratamientoArbolRepositorio extends CrudRepository<TratamientoArbolVO, Integer>{
+public interface TratamientoArbolRepository extends JpaRepository<TratamientoArbolVO, Integer>{
 
 	@Query("select c from TratamientoArbolVO c where arbol=:arbol")
 	Optional<List<TratamientoArbolVO>> findAllByArbol(ArbolVO arbol);
