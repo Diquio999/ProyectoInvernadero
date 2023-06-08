8302507f39dec4adc2d9679ffad314cc0f04c90d
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:54:09 +0200

    Update ServicioFincaImpl.java

--- src/main/java/com/dawes/servicioImpl/ServicioFincaImpl.java
+++ src/main/java/com/dawes/servicioImpl/ServicioFincaImpl.java
@@ -14,75 +14,29 @@ import com.dawes.servicio.ServicioFinca;
 public class ServicioFincaImpl implements ServicioFinca{
 
 	@Autowired
-	private FincaRepositorio fr;
+	private FincaRepositorio repo;
 	
-	@Override
-	public <S extends FincaVO> S save(S entity) {
-		return fr.save(entity);
+		public FincaVO save(FincaVO finca) {
+		return repo.save(finca);
+
 	}
 
-	@Override
-	public <S extends FincaVO> Iterable<S> saveAll(Iterable<S> entities) {
-		return fr.saveAll(entities);
+		public FincaVO findById(Long id) {
+		Optional<FincaVO> finca = repo.findById(id);
+		if (finca.isPresent()) {
+			return finca.get();
+		}
+		return null;
 	}
 
-	@Override
-	public Optional<FincaVO> findById(Integer id) {
-		return fr.findById(id);
-	}
 
-	@Override
-	public boolean existsById(Integer id) {
-		return fr.existsById(id);
+	public List<FincaVO> findAll() {
+		List<FincaVO> fincas = repo.findAll();
+		return fincas;
 	}
-
-	@Override
-	public Iterable<FincaVO> findAll() {
-		return fr.findAll();
-	}
-
-	@Override
-	public Iterable<FincaVO> findAllById(Iterable<Integer> ids) {
-		return fr.findAllById(ids);
-	}
-
-	@Override
-	public long count() {
-		return fr.count();
-	}
-
-	@Override
 	public void deleteById(Integer id) {
 		fr.deleteById(id);
 		
-	}
-
-	@Override
-	public void delete(FincaVO entity) {
-		fr.delete(entity);
-	}
-
-	@Override
-	public void deleteAllById(Iterable<? extends Integer> ids) {
-		fr.deleteAllById(ids);
-		
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends FincaVO> entities) {
-		fr.deleteAll(entities);
-		
-	}
-
-	@Override
-	public void deleteAll() {
-		fr.deleteAll();
-		
-	}
-
-	@Override
-	public Optional<FincaVO> findByNombre(String nombre) {
-		return fr.findByNombre(nombre);
 	}
 
 }
