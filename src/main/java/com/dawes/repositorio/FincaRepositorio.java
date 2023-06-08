dbbc0cc650f6f8e5b4fe360fcfc4bc2c00484406
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:42:21 +0200

    Update FincaRepositorio.java

--- src/main/java/com/dawes/repositorio/FincaRepositorio.java
+++ src/main/java/com/dawes/repositorio/FincaRepositorio.java
@@ -1,14 +1,11 @@
 package com.dawes.repositorio;
 
-import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.dawes.modelo.FincaVO;
-import com.dawes.modelo.RecoleccionVO;
 
 @Repository
-public interface FincaRepositorio extends CrudRepository<FincaVO, Integer>{
-	Optional<FincaVO> findByNombre(String nombre);
+public interface FincaRepository extends JpaRepository<FincaVO, Long> {
+
 }
