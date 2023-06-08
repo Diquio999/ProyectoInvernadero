466943a0473656ff40f576abf503aba2f4a3727b
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:27:01 +0200

    Update RolVO.java

--- src/main/java/com/dawes/modeloUsuarios/RolVO.java
+++ src/main/java/com/dawes/modeloUsuarios/RolVO.java
@@ -1,22 +1,6 @@
 package com.dawes.modeloUsuarios;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+public enum RolVO {
 
-@Entity
-@Table(name="roles")
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-public class RolVO {
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private int idrol;
-	private String nombre;
+	ADMIN,USER;
 }
