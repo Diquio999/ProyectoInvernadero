1198a2430f0512e90cc3e0abaff8a6c74511cb1f
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:43:16 +0200

    Update RecoleccionRepositorio.java

--- src/main/java/com/dawes/repositorio/RecoleccionRepositorio.java
+++ src/main/java/com/dawes/repositorio/RecoleccionRepositorio.java
@@ -1,16 +1,13 @@
 package com.dawes.repositorio;
 
-import java.util.List;
-import java.util.Optional;
 
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.dawes.modelo.FincaVO;
 import com.dawes.modelo.RecoleccionVO;
-
 @Repository
-public interface RecoleccionRepositorio extends CrudRepository<RecoleccionVO, Integer>{	
-	Optional<List<RecoleccionVO>> findByFinca(FincaVO finca);
+public interface RecoleccionRepository extends JpaRepository<RecoleccionVO, Long>{
+
 }
