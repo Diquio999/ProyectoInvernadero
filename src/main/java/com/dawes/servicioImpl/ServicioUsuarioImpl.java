081f600625d78bcc02a7365a71f90ff461c6ba53
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:32:52 +0200

    Update ServicioUsuarioImpl.java

--- src/main/java/com/dawes/servicioImpl/ServicioUsuarioImpl.java
+++ src/main/java/com/dawes/servicioImpl/ServicioUsuarioImpl.java
@@ -5,8 +5,8 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
 import com.dawes.repositorio.UsuarioRepositorio;
+import com.dawes.modeloUsuarios.UsuarioVO;
 
 @Service
 public class ServicioUsuarioImpl implements UserDetailsService {
