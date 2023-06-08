e6bf905db3806f279d77fa8624d48682f37d7256
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:43:40 +0200

    Update FincaRepositorio.java

--- src/main/java/com/dawes/repositorio/FincaRepositorio.java
+++ src/main/java/com/dawes/repositorio/FincaRepositorio.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.dawes.modelo.FincaVO;
 
 @Repository
-public interface FincaRepository extends JpaRepository<FincaVO, Long> {
+public interface FincaRepository extends JpaRepository<FincaVO, Integer> {
 
 }
