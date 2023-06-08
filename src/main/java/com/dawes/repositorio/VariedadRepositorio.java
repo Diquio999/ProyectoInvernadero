b9338a230597db7984178f232c51eabb03f4be34
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 01:24:53 +0200

    Update VariedadRepositorio.java

--- src/main/java/com/dawes/repositorio/VariedadRepositorio.java
+++ src/main/java/com/dawes/repositorio/VariedadRepositorio.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Repository;
 import com.dawes.modelo.VariedadVO;
 
 @Repository
-public interface VariedadRepositorio extends JpaRepository<VariedadVO, Long>{
+public interface VariedadRepositorio extends JpaRepository<VariedadVO, Integer>{
 
 }
