5405a03ee3d41cf2921e329ed8ee53bbf47522e6
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:11:17 +0200

    Update Application.java

--- src/main/java/com/dawes/Application.java
+++ src/main/java/com/dawes/Application.java
@@ -3,7 +3,6 @@ package com.dawes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableJpaRepositories
 @SpringBootApplication
 public class Application {
 
