109e2446bae9855186be796578ed11067898b810
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:11:45 +0200

    Update UsuarioRepositorio.java

--- src/main/java/com/dawes/repositorio/UsuarioRepositorio.java
+++ src/main/java/com/dawes/repositorio/UsuarioRepositorio.java
@@ -6,7 +6,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import com.dawes.modeloUsuarios.UsuarioVO;
-
+@EnableJpaRepositories
 @Repository
 public interface UsuarioRepositorio extends CrudRepository<UsuarioVO, Integer> {
 	UserDetails findByUsername(String username);
