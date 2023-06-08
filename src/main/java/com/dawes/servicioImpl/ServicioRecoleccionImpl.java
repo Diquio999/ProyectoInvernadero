4149506326eb0f5b158d323956c820e43249e474
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:55:33 +0200

    Update ServicioRecoleccionImpl.java

--- src/main/java/com/dawes/servicioImpl/ServicioRecoleccionImpl.java
+++ src/main/java/com/dawes/servicioImpl/ServicioRecoleccionImpl.java
@@ -15,71 +15,39 @@ import com.dawes.servicio.ServicioRecoleccion;
 public class ServicioRecoleccionImpl implements ServicioRecoleccion{
 
 	@Autowired
-	private RecoleccionRepositorio rr;
+	private RecoleccionRepositorio repo;
 	
-	@Override
-	public <S extends RecoleccionVO> S save(S entity) {
-		return rr.save(entity);
+		public RecoleccionVO save(RecoleccionVO finca) {
+		return repo.save(finca);
+
 	}
 
-	@Override
-	public <S extends RecoleccionVO> Iterable<S> saveAll(Iterable<S> entities) {
-		return rr.saveAll(entities);
+	
+
+		public RecoleccionVO findById(Long id) {
+		Optional<RecoleccionVO> recoleccion = repo.findById(id);
+		if (recoleccion.isPresent()) {
+			return recoleccion.get();
+		}
+		return null;
+	}
+	
+	
+	
+	
+		public List<RecoleccionVO> findAll() {
+		List<RecoleccionVO> recolecciones = repo.findAll();
+		return recolecciones;
 	}
 
-	@Override
-	public Optional<RecoleccionVO> findById(Integer id) {
-		return rr.findById(id);
-	}
 
-	@Override
-	public boolean existsById(Integer id) {
-		return rr.existsById(id);
-	}
+	
 
-	@Override
-	public Iterable<RecoleccionVO> findAll() {
-		return rr.findAll();
-	}
 
-	@Override
-	public Iterable<RecoleccionVO> findAllById(Iterable<Integer> ids) {
-		return rr.findAllById(ids);
-	}
-
-	@Override
-	public long count() {
-		return rr.count();
-	}
-
-	@Override
 	public void deleteById(Integer id) {
 		rr.deleteById(id);
 	}
 
-	@Override
-	public void delete(RecoleccionVO entity) {
-		rr.delete(entity);
-	}
-
-	@Override
-	public void deleteAllById(Iterable<? extends Integer> ids) {
-		rr.deleteAllById(ids);
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends RecoleccionVO> entities) {
-		rr.deleteAll(entities);
-	}
-
-	@Override
-	public void deleteAll() {
-		rr.deleteAll();
-	}
-
-	@Override
-	public Optional<List<RecoleccionVO>> findByFinca(FincaVO finca) {
-		return rr.findByFinca(finca);
-	}
+	
 
 }
