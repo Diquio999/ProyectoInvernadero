532ebb41de09709b76f08ee25b32b04a4d5d2e7d
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:57:00 +0200

    Update ServicioTratamientoArbolImpl.java

--- src/main/java/com/dawes/servicioImpl/ServicioTratamientoArbolImpl.java
+++ src/main/java/com/dawes/servicioImpl/ServicioTratamientoArbolImpl.java
@@ -12,68 +12,36 @@ import com.dawes.servicio.ServicioTratamientoArbol;
 @Service
 public class ServicioTratamientoArbolImpl implements ServicioTratamientoArbol{
 	@Autowired
-	private TratamientoArbolRepositorio tar;
+	private TratamientoArbolRepositorio repo;
 
-	@Override
-	public <S extends TratamientoArbolVO> S save(S entity) {
-		return tar.save(entity);
+		public RecoleccionVO save(RecoleccionVO finca) {
+		return repo.save(finca);
+
 	}
 
-	@Override
-	public <S extends TratamientoArbolVO> Iterable<S> saveAll(Iterable<S> entities) {
-		return tar.saveAll(entities);
+
+	public RecoleccionVO findById(Long id) {
+		Optional<RecoleccionVO> recoleccion = repo.findById(id);
+		if (recoleccion.isPresent()) {
+			return recoleccion.get();
+		}
+		return null;
 	}
 
-	@Override
-	public Optional<TratamientoArbolVO> findById(Integer id) {
-		return tar.findById(id);
+
+	
+		public List<RecoleccionVO> findAll() {
+		List<RecoleccionVO> recolecciones = repo.findAll();
+		return recolecciones;
 	}
 
-	@Override
-	public boolean existsById(Integer id) {
-		return tar.existsById(id);
-	}
+	
 
-	@Override
-	public Iterable<TratamientoArbolVO> findAll() {
-		return tar.findAll();
-	}
-
-	@Override
-	public Iterable<TratamientoArbolVO> findAllById(Iterable<Integer> ids) {
-		return tar.findAllById(ids);
-	}
-
-	@Override
-	public long count() {
-		return tar.count();
-	}
-
-	@Override
+	
 	public void deleteById(Integer id) {
 		tar.deleteById(id);
 		
 	}
 
-	@Override
-	public void delete(TratamientoArbolVO entity) {
-		tar.delete(entity);
-		
-	}
-
-	@Override
-	public void deleteAllById(Iterable<? extends Integer> ids) {
-		tar.deleteAllById(ids);
-		
-	}
-
-	@Override
-	public void deleteAll(Iterable<? extends TratamientoArbolVO> entities) {
-		tar.deleteAll(entities);
-	}
-
-	@Override
-	public void deleteAll() {
-		tar.deleteAll();
-	}
+	
 }
