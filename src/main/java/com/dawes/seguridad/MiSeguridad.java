52ca74ea5f63f6f4b4e86a2ca2b94a7d0fc1d7ca
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 00:24:17 +0200

    Update MiSeguridad.java

--- src/main/java/com/dawes/seguridad/MiSeguridad.java
+++ src/main/java/com/dawes/seguridad/MiSeguridad.java
@@ -9,14 +9,58 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-import com.dawes.services.UsuarioService;
+import com.dawes.
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.dawes.servicioImpl.ServicioUsuarioImpl;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
 	@Autowired
-	UsuarioService su;
+	ServicioUsuarioImpl su;
+
+	public String encriptator(String password) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		return encoder.encode(password);
+	}
+
+	@Autowired
+	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("admin").password("{noop}admin").roles("ADMIN");
+	}
+
+	@Bean
+	public SecurityFilterChain filtrocompleto(HttpSecurity http) throws Exception {
+
+		http.authorizeHttpRequests().requestMatchers("/admin/**").hasRole("ADMIN").and().exceptionHandling()
+				.accessDeniedPage("/403");
+
+		http.authorizeHttpRequests().requestMatchers("/","/finca/homefincas","/variedad/homevariedades", "/tratamiento/hometratamientos", "/recoleccion/homerecolecciones", "/arbol/homearboles", "/finca/mapafinca/**", "/css/**").permitAll().anyRequest().authenticated();
+
+		http.formLogin().loginPage("/login").permitAll();
+
+		http.logout().logoutUrl("/logout").permitAll();
+
+		return http.build();
+	}
+}
+.UsuarioService;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+	@Autowired
+	UsuarioServicioImpl su;
 
 	public String encriptator(String password) {
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
