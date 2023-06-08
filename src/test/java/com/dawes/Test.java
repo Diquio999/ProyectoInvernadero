b96b2647aedb77eec87ce3ca9d3941569a529df8
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 01:28:48 +0200

    Update Test.java

--- src/test/java/com/dawes/Test.java
+++ src/test/java/com/dawes/Test.java
@@ -3,12 +3,12 @@ package com.dawes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.dawes.servicio.ServicioVariedad;
+import com.dawes.servicio.ServicioVariedadImpl;
 
 @SpringBootTest
 class Test {
 
 	@Autowired
-	ServicioVariedad sa;
+	ServicioVariedadImpl sa;
 
 }
