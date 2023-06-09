7ca5e34bd93ee468223f6b7b75ade34f0012c85f
Diquio999 <95873338+Diquio999@users.noreply.github.com>
2023-06-09 08:48:12 +0200

    Update VariedadVO.java

--- src/main/java/com/dawes/modelo/VariedadVO.java
+++ src/main/java/com/dawes/modelo/VariedadVO.java
@@ -8,9 +8,7 @@ import jakarta.persistence.UniqueConstraint;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
